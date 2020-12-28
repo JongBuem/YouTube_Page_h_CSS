491518eb44db575b6506b77c6bf9288745754077
JongBuem <75786010+JongBuem@users.noreply.github.com>
2020-12-29 04:59:23 +0900

    입력창 이벤트 변경

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// 제목 화살표 버튼
 const butt = document.querySelector('.title_bt');
 const button_move = document.querySelector('.title span');
 
@@ -22,15 +23,26 @@ const button = {
     }
 }
 
+// 검색창 버튼
 const icons_input = document.querySelector('.icons_input');
 const icons_bt =document.querySelector('.icons_bt1');
+const header = document.querySelector('.head');
+const serch_header = document.querySelector('.serch_header');
+const serch_bt1 =document.querySelector('.serch_bt1');
+const serch_input = document.querySelector('.serch_input');
 
 function input() {
-    icons_input.style.display='inline';
-      }
-      icons_bt.addEventListener("click",input);
+serch_header.style.display='flex';
+  header.style.display='none';
+}
+function serchInput(){
+    header.style.display='flex';
+    serch_header.style.display='none';
+}
+serch_bt1.addEventListener("click", serchInput)
+icons_bt.addEventListener("click",input);
 
-
+// 좋아요와 싫어요 버튼
 const actions_down = document.querySelector('.fa-thumbs-down');
 const actions_up = document.querySelector('.fa-thumbs-up');
 const button_up = document.querySelector('.bt_up');
@@ -38,53 +50,57 @@ const button_down = document.querySelector('.bt_down');
 let count=1;
 let count_not =1;
 
+function upDown(count,count_not){
+    if(count == 1){
+        console.log(333);
+    }
+    else if(count_not==0){
+        console.log(555);
+    }
+}
+
 function func1() {
     if (count == 1){
-    actions_up.textContent=count;
-    actions_up.style.fontSize = 11+ 'pt';
-    count = --count;
+        actions_up.textContent=count;
+        actions_up.style.fontSize = 11+ 'pt';
+        count = --count;
     }else{
         actions_up.textContent=count;
         actions_up.style.fontSize = 11+ 'pt';
         count = ++count;
     }
+    upDown(count,count_not);
 }
-
-  function func2() {
-     if (count_not == 1){
-    actions_down.textContent=count_not;
-    actions_down.style.fontSize = 11+ 'pt';
-    count_not = --count_not;
+function func2() {
+    if (count_not == 1){
+        actions_down.textContent=count_not;
+        actions_down.style.fontSize = 11+ 'pt';
+        count_not = --count_not;
     }else{
         actions_down.textContent=count_not;
         actions_down.style.fontSize = 11+ 'pt';
         count_not = ++count_not;
     }
+    upDown(count,count_not);
 }
 
-    function up() {
+function up() {
     actions_up.classList.toggle('bule');
-      }
-    function down() {
+}
+
+function down() {
     actions_down.classList.toggle('bulee');
-     }
-     function down_up(){
-        actions_up.removeEventListener("click",up);   
-     }
+}
+
+function down_up(){
+    actions_up.removeEventListener("click",up);   
+}
+
 actions_up.addEventListener("click",up);
 actions_up.addEventListener("click",func1);
 actions_down.addEventListener("click",down);
 actions_down.addEventListener("click",func2);
 
-  
-
-
-/*
-actions_down.addEventListener("click", ()=>{
-    actions_down.classList.toggle('bulee');  
-    func2();
-});
-*/
 
 
 
